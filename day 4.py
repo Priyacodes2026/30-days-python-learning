424b9dfd420acf45a30ebb8499e5b4cf7cdbb4f9
Priyacodes2026 <priya2006malar85@gmail.com>
2026-08-21 22:41:13 +0530

    add day 4.py

---
+++ day 4.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+
+print("Equal to:", a == b)
+print("Not equal to:", a != b)
+print("Greater than:", a > b)
+print("Less than:", a < b)
+print("Greater than or equal to:", a >= b)
+print("Less than or equal to:", a <= b)
