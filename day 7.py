964fff5a298a612dffc224742dc88041e75a9479
Priyacodes2026 <priya2006malar85@gmail.com>
2026-08-25 22:35:42 +0530

    Add Day 7 and Day 8 Python loops

---
+++ day 7.py
@@ -0,0 +1,10 @@
+# For Loop
+for i in range(1, 6):
+    print("For Loop:", i)
+
+# While Loop
+i = 1
+
+while i <= 5:
+    print("While Loop:", i)
+    i += 1
---
+++ day 8.py
@@ -0,0 +1,19 @@
+# BREAK
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Break:", i)
+
+
+# CONTINUE
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Continue:", i)
+
+
+# PASS
+for i in range(1, 4):
+    if i == 2:
+        pass
+    print("Pass:", i)
