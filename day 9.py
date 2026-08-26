b7de61fef8651fa095ad8cb1ebb30c71575d4759
Priyacodes2026 <priya2006malar85@gmail.com>
2026-08-26 17:35:17 +0530

    Add Day 9 Python strings

---
+++ day 9.py
@@ -0,0 +1,11 @@
+# String
+name = "Priya"
+message = "Learning Python"
+
+print("Name:", name)
+print("Message:", message)
+
+# String operations
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("Length:", len(message))
