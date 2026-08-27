09b97ee86b5192b79f2938f6a5c88ceaf741183c
Priyacodes2026 <priya2006malar85@gmail.com>
2026-08-27 21:14:21 +0530

    Add Day 10 Python string methods

---
+++ day10.py
@@ -0,0 +1,8 @@
+text = "  Hello Python  "
+
+print("Original:", text)
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Without extra spaces:", text.strip())
+print("Replaced:", text.replace("Python", "World"))
