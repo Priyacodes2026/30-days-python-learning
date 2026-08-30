ca788b345e654c8ff3a6b71eb5562319d6264c32
Priyacodes2026 <priya2006malar85@gmail.com>
2026-08-30 23:06:04 +0530

    Add Day 13 Python tuples

---
+++ day 13.py
@@ -0,0 +1,17 @@
+# Day 13 - Tuples
+
+student = ("Priya", 21, "Biomedical Engineering")
+
+# Print the tuple
+print("Tuple:", student)
+
+# Access items using index
+print("First item:", student[0])
+print("Second item:", student[1])
+print("Third item:", student[2])
+
+# Find total number of items
+print("Total items:", len(student))
+
+# Tuple values cannot be changed
+print("Tuple values cannot be changed!")
