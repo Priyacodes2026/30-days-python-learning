461ffcbf9434b5e644b7a18b3e00822c1bf20d09
Priyacodes2026 <priya2006malar85@gmail.com>
2026-09-01 13:56:47 +0530

    Add Day 14 Python dictionaries

---
+++ day14.py
@@ -0,0 +1,18 @@
+# Day 14 - Dictionaries
+
+student = {
+    "name": "Priya",
+    "age": 21,
+    "course": "Biomedical Engineering"
+}
+
+# Print dictionary
+print("Student:", student)
+
+# Access values using keys
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+
+# Using get()
+print("Name using get():", student.get("name"))
