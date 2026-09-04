eb18906fad843b4e61cd47ec2bf3a336feb41031
Priyacodes2026 <priya2006malar85@gmail.com>
2026-09-04 22:45:51 +0530

    Add Day 16 Python sets

---
+++ day16.py
@@ -0,0 +1,16 @@
+# Day 16 - Sets
+
+numbers = {1, 2, 3, 4, 2, 3, 5}
+
+print("Set:", numbers)
+
+# Adding a new value
+numbers.add(6)
+print("After adding:", numbers)
+
+# Removing a value
+numbers.remove(3)
+print("After removing:", numbers)
+
+# Set length
+print("Total unique values:", len(numbers))
